fe205ebb5a27001de8989ced793141df70b74e65
Fred Song <fsong@xei.ca>
2013-02-26 20:00:35 -0800

    splitting up bencode test cases

--- tests/bencode_test.cpp
+++ tests/bencode_test.cpp
@@ -1,20 +1,40 @@
+/*Tests bencoding function works correctly*/
+
 #include <cassert>
 #include <cstring>
 #include <vector>
 #include "bencode.h"
 
-int main() {
+void test_bencode_string() {
     char str_tok[] = "3:foo";
-    char int_tok[] = "i:200e";
-    char list_tok[] = "l4:spam4:eggse";
-    char dict_tok[] = "d3:cow3:moo4:spam4:eggse";
-    char combined_tok[] = "3:bari:2el:i:3eed:3:cow3:mooe";
-
-    // TODO: fix object slicing problem
     std::vector<BencodeTokenPtr> lst = parseBencode(str_tok, strlen(str_tok)+1);
     assert(lst.size() == 1);
     assert(lst[0]->type == BE_STRING);
     assert(
         ((BencodeString *)lst[0].get())->value == "foo"
     );
+}
+
+void test_bencode_integer() {
+    char int_tok[] = "i:200e";
+
+}
+
+void test_bencode_list() {
+    char list_tok[] = "l4:spam4:eggse";
+}
+
+void test_bencode_dict() {
+    char dict_tok[] = "d3:cow3:moo4:spam4:eggse";
+}
+
+void test_bencode_composite() {
+    char combined_tok[] = "3:bari:2el:i:3eed:3:cow3:mooe";
+}
+
+int main() {
+    test_bencode_string();
+    test_bencode_integer();
+    test_bencode_list();
+    test_bencode_dict();
 }
